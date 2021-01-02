33cdee56a0e17497482d401c95c73302253cf20f
Erfan Abdi <erfangplus@gmail.com>
2021-01-02 18:55:27 +0330

    proc: Replace skip_initramfs to androidboot.force_normal_boot
    Change-Id: I6712cce574e6981577bed6718ad0b3daf57aee2a

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -64,6 +64,27 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+
+	char *search = "skip_initramf";
+	char *replace = "androidboot.force_normal_boot=1 ";
+
+	char *offset_addr, *cmd = proc_command_line;
+	size_t search_len, replace_len;
+
+	strcpy(cmd, saved_command_line);
+
+        search_len = strlen(search);
+        replace_len = strlen(replace);
+
+	offset_addr = strstr(cmd, search);
+	if (offset_addr) {
+		size_t tail_len;
+		tail_len = strlen(offset_addr+search_len);
+
+		memmove(offset_addr+replace_len,offset_addr+search_len,tail_len+1);
+		memcpy(offset_addr, replace, replace_len);
+	}
+
 	proc_command_line_init();
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
